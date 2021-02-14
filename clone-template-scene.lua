d159849aebcdce29f5e644a4fb8b1722f3c9ec28
Ralf S. Engelschall <rse@engelschall.com>
2021-02-14 14:11:49 +0100

    cleanup script

--- clone-template-scene.lua
+++ clone-template-scene.lua
@@ -22,6 +22,7 @@ local ctx = {
 function script_description ()
     return [[
         <h2>Clone Template Scene</h2>
+
         Copyright &copy; 2021 <a style="color: #ffffff; text-decoration: none;"
         href="http://engelschall.com">Dr. Ralf S. Engelschall</a><br/>
         Distributed under <a style="color: #ffffff; text-decoration: none;"
@@ -33,22 +34,22 @@ function script_description ()
         their filters, transforms, etc.</b>
 
         <p>
-        <u>Notice:</u> The same cloning <i>cannot</i> to be achieved
+        <u>Notice:</u> The same kind of cloning <i>cannot</i> to be achieved
         manually, as the scene <i>Duplicate</i> and the source
         <i>Copy</i> functions create references for many source types
         only and especially do not clone applied transforms. The only
         alternative is the tedious process of creating a new scene,
-        copying and pasting all sources and then also copying and
-        pasting all source transforms.
+        step-by-step copying and pasting all sources and then also
+        step-by-step copying and pasting all source transforms.
 
         <p>
         <u>Prerequisite:</u> This script assumes that the source
-        scene is named <tt>"XXX"</tt> (e.g. <tt>"Template-01"</tt>),
-        all of its sources are named <tt>"XXX-ZZZ"</tt> (e.g.
-        <tt>"Template-01-Placeholder-02"</tt>), the target scene is
-        named <tt>"YYY"</tt> (e.g. <tt>"Scene-03"</tt>) and all of
-        its sources are consequently named <tt>"YYY-ZZZ"</tt> (e.g.
-        <tt>"Scene-03-Placeholder-02"</tt>).
+        scene is named <tt>XXX</tt> (e.g. <tt>Template-01</tt>),
+        all of its sources are named <tt>XXX-ZZZ</tt> (e.g.
+        <tt>Template-01-Placeholder-02</tt>), the target scene is
+        named <tt>YYY</tt> (e.g. <tt>Scene-03</tt>) and all of
+        its sources are consequently named <tt>YYY-ZZZ</tt> (e.g.
+        <tt>Scene-03-Placeholder-02</tt>).
     ]]
 end
 
@@ -73,62 +74,70 @@ end
 --  script hook: define UI properties
 function script_properties ()
     --  create new properties
-    local props = obs.obs_properties_create()
-    ctx.propsDef = props
+    ctx.propsDef = obs.obs_properties_create()
 
     --  create source scene list
-    ctx.propsDefSrc = obs.obs_properties_add_list(props,
+    ctx.propsDefSrc = obs.obs_properties_add_list(ctx.propsDef,
         "sourceScene", "Source Scene (Template):",
         obs.OBS_COMBO_TYPE_LIST, obslua.OBS_COMBO_FORMAT_STRING)
     updateSourceScenes()
 
     --  create target scene field
-    obs.obs_properties_add_text(props, "targetScene",
+    obs.obs_properties_add_text(ctx.propsDef, "targetScene",
         "Target Scene (Clone):", obs.OBS_TEXT_DEFAULT)
 
     --  create clone button
-    obs.obs_properties_add_button(props, "clone",
-        "Clone Template Scene", do_clone)
+    obs.obs_properties_add_button(ctx.propsDef, "clone",
+        "Clone Template Scene", doClone)
 
     --  create status field (read-only)
-    local status = obs.obs_properties_add_text(props, "statusMessage", "Status Message:", obs.OBS_TEXT_MULTILINE)
-	obs.obs_property_set_enabled(status, false)
+    local status = obs.obs_properties_add_text(ctx.propsDef, "statusMessage",
+        "Status Message:", obs.OBS_TEXT_MULTILINE)
+    obs.obs_property_set_enabled(status, false)
 
-    --  
-    obs.obs_properties_apply_settings(props, ctx.propsSet)
-    return props
+    --  apply values to definitions
+    obs.obs_properties_apply_settings(ctx.propsDef, ctx.propsSet)
+    return ctx.propsDef
 end
 
---  script hook: property values were updated
-function script_update (settings)
-    ctx.propsSet = settings
-	ctx.propsVal.sourceScene   = obs.obs_data_get_string(settings, "sourceScene")
-	ctx.propsVal.targetScene   = obs.obs_data_get_string(settings, "targetScene")
-	ctx.propsVal.statusMessage = obs.obs_data_get_string(settings, "statusMessage")
-end
-
---  configure property defaults
+--  script hook: define property defaults
 function script_defaults (settings)
+    --  update our source scene list (for propsValSrc below)
     updateSourceScenes()
+
+    --  provide default values
     obs.obs_data_set_default_string(settings, "sourceScene",   ctx.propsValSrc)
     obs.obs_data_set_default_string(settings, "targetScene",   "Scene-01")
     obs.obs_data_set_default_string(settings, "statusMessage", "")
 end
 
-function onEvent (event)
-    if event == obs.OBS_FRONTEND_EVENT_SCENE_LIST_CHANGED then
-        updateSourceScenes()
-    end
-    return true
+--  script hook: property values were updated
+function script_update (settings)
+    --  remember settings
+    ctx.propsSet = settings
+
+    --  fetch property values
+    ctx.propsVal.sourceScene   = obs.obs_data_get_string(settings, "sourceScene")
+    ctx.propsVal.targetScene   = obs.obs_data_get_string(settings, "targetScene")
+    ctx.propsVal.statusMessage = obs.obs_data_get_string(settings, "statusMessage")
 end
 
 --  react on script load
 function script_load (settings)
+    --  clear status message
     obs.obs_data_set_string(settings, "statusMessage", "")
-    obs.obs_frontend_add_event_callback(onEvent)
+
+    --  react on scene list changes
+    obs.obs_frontend_add_event_callback(function (event)
+        if event == obs.OBS_FRONTEND_EVENT_SCENE_LIST_CHANGED then
+            --  update our source scene list
+            updateSourceScenes()
+        end
+        return true
+    end)
 end
 
---  utility function for setting status message
+--  helper function: set status message
 local function statusMessage (type, message)
     if type == "error" then
         obs.script_log(obs.LOG_INFO, message)
@@ -141,7 +150,7 @@ local function statusMessage (type, message)
     return true
 end
 
---  utility function for finding scene by name
+--  helper function: find scene by name
 local function findSceneByName (name)
     local scenes = obs.obs_frontend_get_scenes()
     if scenes == nil then
@@ -158,31 +167,35 @@ local function findSceneByName (name)
     return nil
 end
 
+--  helper function: replace a string
 local function stringReplace (str, from, to)
-	local function regexEscape (s)
-		return string.gsub(s, "[%(%)%.%%%+%-%*%?%[%^%$%]]", "%%%1")
-	end
+    local function regexEscape (s)
+        return string.gsub(s, "[%(%)%.%%%+%-%*%?%[%^%$%]]", "%%%1")
+    end
     return string.gsub(str, regexEscape(from), to)
 end
 
---  called for cloning action
-function do_clone ()
+--  called for the actual cloning action
+function doClone ()
     --  find source scene (template)
     local sourceScene = findSceneByName(ctx.propsVal.sourceScene)
     if sourceScene == nil then
-        statusMessage("error", string.format("source scene \"%s\" not found!", ctx.propsVal.sourceScene))
+        statusMessage("error", string.format("source scene \"%s\" not found!",
+            ctx.propsVal.sourceScene))
         return true
     end
 
     --  find target scene (clone)
     local targetScene = findSceneByName(ctx.propsVal.targetScene)
     if targetScene ~= nil then
-        statusMessage("error", string.format("target scene \"%s\" already exists!", ctx.propsVal.targetScene))
+        statusMessage("error", string.format("target scene \"%s\" already exists!",
+            ctx.propsVal.targetScene))
         return true
     end
 
     --  create target scene
-    obs.script_log(obs.LOG_INFO, string.format("create: SCENE  \"%s\"", ctx.propsVal.targetScene))
+    obs.script_log(obs.LOG_INFO, string.format("create: SCENE  \"%s\"",
+        ctx.propsVal.targetScene))
     targetScene = obs.obs_scene_create(ctx.propsVal.targetScene)
 
     --  iterate over all source scene (template) sources
@@ -193,8 +206,10 @@ function do_clone ()
 
         --  determine source and destination name
         local sourceNameSrc = obs.obs_source_get_name(sourceSrc)
-        local sourceNameDst = stringReplace(sourceNameSrc, ctx.propsVal.sourceScene, ctx.propsVal.targetScene)
-        obs.script_log(obs.LOG_INFO, string.format("create: SOURCE \"%s/%s\"", ctx.propsVal.targetScene, sourceNameDst))
+        local sourceNameDst = stringReplace(sourceNameSrc,
+            ctx.propsVal.sourceScene, ctx.propsVal.targetScene)
+        obs.script_log(obs.LOG_INFO, string.format("create: SOURCE \"%s/%s\"",
+            ctx.propsVal.targetScene, sourceNameDst))
 
         --  create source
         local type = obs.obs_source_get_id(sourceSrc)
@@ -204,39 +219,39 @@ function do_clone ()
         --  add source to scene
         local targetItem = obs.obs_scene_add(targetScene, targetSource)
 
-        --  copy transforms
+        --  copy source transforms
         local transform = obs.obs_transform_info()
         obs.obs_sceneitem_get_info(sourceItem, transform)
         obs.obs_sceneitem_set_info(targetItem, transform)
 
-        --  copy filters
+        --  copy source filters
         obs.obs_source_copy_filters(targetSource, sourceSrc)
 
-        --  copy volume
+        --  copy source volume
         local volume = obs.obs_source_get_volume(sourceSrc)
         obs.obs_source_set_volume(targetSource, volume)
 
-        --  copy muted state
+        --  copy source muted state
         local muted = obs.obs_source_muted(sourceSrc)
         obs.obs_source_set_muted(targetSource, muted)
 
-        --  copy mixer state
+        --  copy source mixer state
         local mixers = obs.obs_source_get_audio_mixers(sourceSrc)
         obs.obs_source_set_audio_mixers(targetSource, mixers)
 
-        --  copy flags
+        --  copy source flags
         local flags = obs.obs_source_get_flags(sourceSrc)
         obs.obs_source_set_flags(targetSource, flags)
 
-        --  copy enabled state
+        --  copy source enabled state
         local enabled = obs.obs_source_enabled(sourceSrc)
         obs.obs_source_set_enabled(targetSource, enabled)
 
-        --  copy visible state
+        --  copy source visible state
         local visible = obs.obs_sceneitem_visible(sourceItem)
         obs.obs_sceneitem_set_visible(targetItem, visible)
 
-        --  copy locked state
+        --  copy source locked state
         local locked = obs.obs_sceneitem_locked(sourceItem)
         obs.obs_sceneitem_set_locked(targetItem, locked)
 

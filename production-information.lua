fe6839eec97b1eb706feef5d74c39a4210288cf2
Ralf S. Engelschall <rse@engelschall.com>
2021-04-02 14:20:59 +0200

    final cleanups

--- production-information.lua
+++ production-information.lua
@@ -1,6 +1,6 @@
 --[[
 **
-**  current-scene-information.lua -- OBS Studio Lua Script for Current Scene Information
+**  production-information.lua -- OBS Studio Lua Script for Production Information
 **  Copyright (c) 2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **  Distributed under MIT license <https://spdx.org/licenses/MIT.html>
 **
@@ -11,6 +11,7 @@ local obs = obslua
 
 --  global context information
 local ctx = {
+    --  properties
     propsDef            = nil,   -- property definition
     propsDefSrcPreview  = nil,   -- property definition (source scene of preview)
     propsDefSrcProgram  = nil,   -- property definition (source scene of program)
@@ -22,9 +23,13 @@ local ctx = {
     propsValSrcProgram  = nil,   -- property values (source scene of program)
     propsValSrcTime     = nil,   -- property values (source scene of time)
     propsValSrcDuration = nil,   -- property values (source scene of duration)
+
+    -- duration timer
 	timerStart          = 0,     -- timer start (in nannoseconds)
 	timerPaused         = false, -- timer paused flag
 	timerPausedSecs     = 0,     -- timer paused time (in seconds)
+
+    -- hotkey registration
     hotkeyIdPause       = obs.OBS_INVALID_HOTKEY_ID,
     hotkeyIdReset       = obs.OBS_INVALID_HOTKEY_ID
 }
@@ -32,7 +37,7 @@ local ctx = {
 --  script hook: description displayed on script window
 function script_description ()
     return [[
-        <h2>Current Scene Information</h2>
+        <h2>Production Information</h2>
 
         Copyright &copy; 2021 <a style="color: #ffffff; text-decoration: none;"
         href="http://engelschall.com">Dr. Ralf S. Engelschall</a><br/>
@@ -40,24 +45,25 @@ function script_description ()
         href="https://spdx.org/licenses/MIT.html">MIT license</a>
 
         <p>
-        <b>Render current Preview and Program scene names into corresponding text sources.</b>
+        <b>Render production information into corresponding text sources.</b>
 
         <p>
         This is a small OBS Studio script for rendering the current
-        scene names visible in the Preview and Program channels, the
-        current wallclock time and the current on-air duration time
-        into pre-defined corresponding Text/GDI+ text sources. These
-        text sources are usually part of an (invisible) scene which is
-        either part of a locally shown OBS Studio Multiview or Projector
-        or is broacasted via an attached "Dedicated NDI Output" filter
+        scene name visible in the Preview and Program channels, the
+        current wallclock time and the current on-air duration time into
+        pre-defined corresponding Text/GDI+ text sources. These text
+        sources are usually part of a (hidden) scene which is either
+        just part of a locally shown OBS Studio Multiview or Projector
+        or is broadcasted via an attached "Dedicated NDI Output" filter
         to foreign monitors. In all cases, the intention is to globally
-        show the current production information to all involved people
+        show current production information to the involved people
         during a production session.
     ]]
 end
 
 --  helper function: update text source properties
 function updateTextSources ()
+    --  clear already initialized property lists
     if ctx.propsDefSrcPreview ~= nil then
         obs.obs_property_list_clear(ctx.propsDefSrcPreview)
     end
@@ -70,34 +76,51 @@ function updateTextSources ()
     if ctx.propsDefSrcDuration ~= nil then
         obs.obs_property_list_clear(ctx.propsDefSrcDuration)
     end
+
+    --  clear all selected propety values
     ctx.propsValSrcPreview  = nil
     ctx.propsValSrcProgram  = nil
     ctx.propsValSrcTime     = nil
     ctx.propsValSrcDuration = nil
+
+    --  iterate over all text sources...
 	local sources = obs.obs_enum_sources()
 	if sources ~= nil then
 		for _, source in ipairs(sources) do
 			source_id = obs.obs_source_get_unversioned_id(source)
             if source_id == "text_gdiplus" or source_id == "text_ft2_source" then
+                --  ...and fetch their source names
                 local name = obs.obs_source_get_name(source)
+
+                --  add source to preview text source selection list
+                --  and initialize selected value
                 if ctx.propsDefSrcPreview ~= nil then
                     obs.obs_property_list_add_string(ctx.propsDefSrcPreview, name, name)
                 end
                 if ctx.propsValSrcPreview == nil then
                     ctx.propsValSrcPreview = name
                 end
+
+                --  add source to program text source selection list
+                --  and initialize selected value
                 if ctx.propsDefSrcProgram ~= nil then
                     obs.obs_property_list_add_string(ctx.propsDefSrcProgram, name, name)
                 end
                 if ctx.propsValSrcProgram == nil then
                     ctx.propsValSrcProgram = name
                 end
+
+                --  add source to time text source selection list
+                --  and initialize selected value
                 if ctx.propsDefSrcTime ~= nil then
                     obs.obs_property_list_add_string(ctx.propsDefSrcTime, name, name)
                 end
                 if ctx.propsValSrcTime == nil then
                     ctx.propsValSrcTime = name
                 end
+
+                --  add source to duration text source selection list
+                --  and initialize selected value
                 if ctx.propsDefSrcDuration ~= nil then
                     obs.obs_property_list_add_string(ctx.propsDefSrcDuration, name, name)
                 end
@@ -126,7 +149,7 @@ function script_properties ()
     --  create new properties
     local props = obs.obs_properties_create()
 
-    --  create two inputs
+    --  create selection fields
     ctx.propsDefSrcPreview = obs.obs_properties_add_list(props,
         "textSourceNamePreview", "Preview-Name Text-Source",
         obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
@@ -139,6 +162,9 @@ function script_properties ()
     ctx.propsDefSrcDuration = obs.obs_properties_add_list(props,
         "textSourceNameDuration", "Duration-Time Text-Source",
         obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+    updateTextSources()
+
+    --  create buttons
 	obs.obs_properties_add_button(props, "buttonStartStop", "Duration: Start/Stop", function ()
         durationPause()
 	    return true
@@ -147,7 +173,6 @@ function script_properties ()
         durationReset()
 	    return true
     end)
-    updateTextSources()
 
     return props
 end
@@ -180,7 +205,6 @@ end
 function updateTextSource (name, text)
     local source = obs.obs_get_source_by_name(name)
     if source ~= nil then
-        --  obs.script_log(obs.LOG_INFO, "update: change Text source \"" .. name .. "\" to \"" .. text .. "\"")
         local settings = obs.obs_source_get_settings(source)
         obs.obs_data_set_string(settings, "text", text)
         obs.obs_source_update(source, settings)
@@ -269,6 +293,7 @@ end
 
 --  script hook: on script save state
 function script_save(settings)
+    --  save hotkeys
 	local hotkeyArrayPause = obs.obs_hotkey_save(ctx.hotkeyIdPause)
 	local hotkeyArrayReset = obs.obs_hotkey_save(ctx.hotkeyIdReset)
 	obs.obs_data_set_array(settings, "hotkey_pause", hotkeyArrayPause)

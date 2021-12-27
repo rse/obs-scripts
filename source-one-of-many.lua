ff448386d2ca55ba11c480d7f60bde2d786d335c
Ralf S. Engelschall <rse@engelschall.com>
2021-12-27 20:45:11 +0100

    make luacheck happy again

--- source-one-of-many.lua
+++ source-one-of-many.lua
@@ -16,9 +16,83 @@ local ctx = {
 }
 
 --  send a trace message to the script log
-function script_log (level, msg)
+local function script_log (level, msg)
     local time = os.date("%Y-%m-%d %X")
     obs.script_log(obs.LOG_INFO, string.format("%s [%s] %s\n", time, level, msg))
+end
+
+--  callback of "item_visible" handler (scene/source visibility changed)
+local function cb_item_visible (calldata)
+    --  determine current callback information
+    local item    = obs.calldata_sceneitem(calldata, "item")
+    local visible = obs.calldata_bool(calldata,      "visible")
+
+    --  determine changed scene/source name
+    local source = obs.obs_sceneitem_get_source(item)
+    local sourceName = obs.obs_source_get_name(source)
+    if visible then
+        script_log("INFO", string.format("notice: source \"%s\" will be made visible (by OBS)", sourceName))
+    else
+        script_log("INFO", string.format("notice: source \"%s\" will be made non-visible (by OBS)", sourceName))
+    end
+
+    --  iterate over all scenes of scene/source
+    local scene = obs.obs_sceneitem_get_scene(item)
+    local sceneitems = obs.obs_scene_enum_items(scene)
+    local found_other_visible = false
+    for i, sceneitem in ipairs(sceneitems) do
+        local itemsource = obs.obs_sceneitem_get_source(sceneitem)
+        local name = obs.obs_source_get_name(itemsource)
+
+        --  for another source which is visible...
+        if sourceName ~= name and obs.obs_sceneitem_visible(sceneitem) then
+            if visible then
+                --  ...make it non-visible
+                script_log("INFO", string.format("forcing source \"%s\" to be non-visible (by us)", name))
+                obs.obs_sceneitem_set_visible(sceneitem, false)
+            else
+                --  ...or remember it is visible
+                found_other_visible = true
+            end
+        end
+    end
+    obs.sceneitem_list_release(sceneitems)
+
+    --  if a source is requested to be made non-visible and we have not
+    --  found any other still visible source in the scene/group, make
+    --  it visible again (but delay the toggling as the source is still
+    --  just flagged to be made non-visible and is actually just to be
+    --  made non-visible after this callback!)
+    if not visible and not found_other_visible then
+        script_log("INFO", string.format("forcing source \"%s\" to be visible again afterwards (by us)", sourceName))
+        table.insert(ctx.set_visible, { item = item, delay = 10, visible = true })
+    end
+end
+
+--  callback of "source_load" handler (called when a source is being loaded)
+local function cb_source_load (calldata)
+    --  skip operation of no global settings are available
+    if ctx.settings == nil then
+        return
+    end
+
+    --  determine current loaded source
+    local source = obs.calldata_source(calldata, "source")
+    local sn = obs.obs_source_get_name(source)
+
+    --  (re)connect "item_visible" handler if one of our configured sources is loaded
+    local sourceNames = obs.obs_data_get_array(ctx.settings, "sources")
+    local sourceNamesCount = obs.obs_data_array_count(sourceNames)
+    for i = 0, sourceNamesCount do
+        local item = obs.obs_data_array_item(sourceNames, i)
+        local sourceName = obs.obs_data_get_string(item, "value")
+        if sn == sourceName then
+            local sh = obs.obs_source_get_signal_handler(source)
+            obs.signal_handler_disconnect(sh, "item_visible", cb_item_visible)
+            obs.signal_handler_connect(sh,    "item_visible", cb_item_visible)
+        end
+    end
+    obs.obs_data_array_release(sourceNames)
 end
 
 --  script hook: description displayed on script window
@@ -90,80 +164,6 @@ function script_tick (seconds)
         else
             i = i + 1
         end
-    end
-end
-
---  callback of "source_load" handler (called when a source is being loaded)
-function cb_source_load (calldata)
-    --  skip operation of no global settings are available
-    if ctx.settings == nil then
-        return
-    end
-
-    --  determine current loaded source
-    local source = obs.calldata_source(calldata, "source")
-    local sn = obs.obs_source_get_name(source)
-
-    --  (re)connect "item_visible" handler if one of our configured sources is loaded
-    local sourceNames = obs.obs_data_get_array(ctx.settings, "sources")
-    local sourceNamesCount = obs.obs_data_array_count(sourceNames)
-    for i = 0, sourceNamesCount do
-        local item = obs.obs_data_array_item(sourceNames, i)
-        local sourceName = obs.obs_data_get_string(item, "value")
-        if sn == sourceName then
-            local sh = obs.obs_source_get_signal_handler(source)
-            obs.signal_handler_disconnect(sh, "item_visible", cb_item_visible)
-            obs.signal_handler_connect(sh,    "item_visible", cb_item_visible)
-        end
-    end
-    obs.obs_data_array_release(sourceNames)
-end
-
---  callback of "item_visible" handler (scene/source visibility changed)
-function cb_item_visible (calldata)
-    --  determine current callback information
-    local item    = obs.calldata_sceneitem(calldata, "item")
-    local visible = obs.calldata_bool(calldata,      "visible")
-
-    --  determine changed scene/source name
-    local source = obs.obs_sceneitem_get_source(item)
-    local sourceName = obs.obs_source_get_name(source)
-    if visible then
-        script_log("INFO", string.format("notice: source \"%s\" will be made visible (by OBS)", sourceName))
-    else
-        script_log("INFO", string.format("notice: source \"%s\" will be made non-visible (by OBS)", sourceName))
-    end
-
-    --  iterate over all scenes of scene/source
-    local scene = obs.obs_sceneitem_get_scene(item)
-    local sceneitems = obs.obs_scene_enum_items(scene)
-    local found_other_visible = false
-    for i, sceneitem in ipairs(sceneitems) do
-        local itemsource = obs.obs_sceneitem_get_source(sceneitem)
-        local name = obs.obs_source_get_name(itemsource)
-
-        --  for another source which is visible...
-        if sourceName ~= name and obs.obs_sceneitem_visible(sceneitem) then
-            if visible then
-                --  ...make it non-visible
-                script_log("INFO", string.format("forcing source \"%s\" to be non-visible (by us)", name))
-                obs.obs_sceneitem_set_visible(sceneitem, false)
-            else
-                --  ...or remember it is visible
-                found_other_visible = true
-            end
-        end
-    end
-    obs.sceneitem_list_release(sceneitems)
-
-    --  if a source is requested to be made non-visible and we have not
-    --  found any other still visible source in the scene/group, make
-    --  it visible again (but delay the toggling as the source is still
-    --  just flagged to be made non-visible and is actually just to be
-    --  made non-visible after this callback!)
-    if not visible and not found_other_visible then
-        script_log("INFO", string.format("forcing source \"%s\" to be visible again afterwards (by us)", sourceName))
-        table.insert(ctx.set_visible, { item = item, delay = 10, visible = true })
     end
 end
 
